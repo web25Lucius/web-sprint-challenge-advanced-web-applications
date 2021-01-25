f622819f6b648de5395ac13046fe937795889735
web25Lucius <web25Lucius@gmail.com>
2021-01-24 22:20:30 -0700

    fixed rendering error by adding sass dependency, creating log in

--- client/src/components/BubblePage.js
+++ client/src/components/BubblePage.js
@@ -1,11 +1,24 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
-
+import {axiosWithAuth } from "./AuthHeader";
 import Bubbles from "./Bubbles";
 import ColorList from "./ColorList";
+import { response } from "express";
 
 const BubblePage = () => {
   const [colorList, setColorList] = useState([]);
+  useEffect(()=>{
+    axiosWithAuth()
+    .get(server)
+    .then((response) => {
+     response = response => setColorList[response.colorList]
+    }
+)
+  .catch((err)=> {
+    console.log((err)
+  )})}, [])
+
+
   // fetch your colors data from the server when the component mounts
   // set that data to the colorList state property
 
